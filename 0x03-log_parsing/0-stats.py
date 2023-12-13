e83719399809e4f8c2b6bdb5656f5fb3a41e6959
Caesar-12 <idowuenoch65@gmail.com>
2023-12-13 13:16:48 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
+"""Log parsing"""
+
+
 import sys
 
-# store the count of all status codes in a dictionary
 status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
 
 total_size = 0
-count = 0  # keep count of the number lines counted
+count = 0
 
 try:
     for line in sys.stdin:
@@ -15,22 +18,17 @@ try:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
             if status_code in status_codes_dict.keys():
                 status_codes_dict[status_code] += 1
 
-            # update total size
             total_size += file_size
 
-            # update count of lines
             count += 1
 
         if count == 10:
-            count = 0  # reset count
+            count = 0
             print('File size: {}'.format(total_size))
 
-            # print out status code counts
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
